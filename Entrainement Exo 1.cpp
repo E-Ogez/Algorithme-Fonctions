450022fbd8e9dae87c48c56885678de3859bd9d0
E-Ogez <90181762+E-Ogez@users.noreply.github.com>
2021-10-19 11:12:49 +0200

    Add files via upload

---
+++ Entrainement Exo 1.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+
+int main() {
+
+	int n = 0, m = 0;
+
+		printf("Valeur Entier : ", n);                                         
+		scanf("%d", &n);                                        
+		m = n;
+		n = abs(n);
+		printf("Valeure Absolu de %d = %d\n", m, n);
+
+		return 0;
+
+}
+
+
+
+
+
+
+
+/* Ecrire une fonction qui calcule la valeur absolue d’un entier. Cette fonction sera
+appelée dans un programme qui affichera ensuite le résultat.  */
+
---
+++ Entrainement Exo 2.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+
+float moyenne(int x, int y);
+
+int main()
+{
+	int x = 0;
+	int y = 0;
+	float m;
+
+	printf("Premiere Valeur (x) : ", x);
+	scanf("%d", &x);
+	printf("Deuzieme Valeur (y) : ", y);
+	scanf("%d", &y);
+
+	m = moyenne(x, y);
+	printf("La moyenne de %d et %d est : %f\n", x, y, m);
+	return 0;
+
+}
+	float moyenne(int x, int y)
+{
+		float m;
+		int s;
+
+		s = x + y;
+		m = s / 2.0;
+
+		printf("a=%d, b=%d, m=%f, s=%d\n", x, y, m, s);
+		return m;
+}
+
+/* Ecrire une fonction qui calcule x puis y. Cette fonction sera appelée dans un
+programme qui affichera ensuite le résultat */
